ee2137ff0c76f42f0512a8b6b9c6b44f650c645e
Manoj Kumar <manoj.code10@gmail.com>
2021-05-09 00:29:45 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,124 @@
+<?php ?>
+<!--HTML boiler plate-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Impetus 21</title>
+    <link rel="icon" href="/logo.png" type = "image/x-icon">
+    <!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+
+    <!--font awesome-->  
+    <!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">-->
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+
+  
+    <!--bootstrap js-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    
+    <!--google font poppins-->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+</head>
+
+<body style="font-family:'Poppins', sans-serif;">
+
+
+  <div id="part1" style="background-color:black;">
+  
+    <nav class="navbar navbar-expand-md navbar-dark" style="background-color:black;color:white;">
+        <a href="#" class="navbar-brand"></a>
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+           <div class="navbar-nav">
+              <a href="#" class="nav-item nav-link active">Home</a>
+              <a href="#" class="nav-item nav-link">About</a>
+           </div>
+        </div>
+  </nav>
+
+
+  <div class="ml-5 mr-5 mt-2 mb-2">
+    <div class="row">
+      <div class="col-12 col-sm-6 text-center">
+      <!--<iframe frameBorder="0" class="res-img" src="https://www.youtube.com/embed/1Eg2gHnAGw8?mute=1&controls=0" title="YouTube video player">
+      </iframe>-->
+      <img class="res-img" src="test1.gif" alt="test1">
+      </div>
+      <div class="col-12 col-sm-6 text-center">
+        <!--<h1 class="responsive-lg mt-2 text-center" style="color: black;
+        text-shadow: -1px -1px 0 #fff, 1px -1px 0 #fff, -1px 1px 0 #fff, 1px 1px 0 #fff;">IMPETUS 21.0</h1>-->
+        <h1 class="responsive-lg font-wight-bold text-center" style="color:white;">IMPETUS 21.0</h1>
+        <button type="button" class="btn btn-light res-pad res-mar rounded-pill" style="background-color: white;">Register</button>
+        <button type="button" class="btn btn-light res-pad res-mar rounded-pill" style="background-color: white;">See what's happening</button>
+        <p id="demo" class="font-weight-bold mt-4 responsive-tx" style="color:white;"></p>
+      </div>
+    </div>
+  </div>
+  <br><br>
+  <div class="ml-4 mr-4 mt-2 mb-2">
+    <!--<h1 class="responsive-md font-wight-bold text-center" style="color:white;">TECHNICAL EXTRAVAGANZA</h1>-->
+    <h1 class="responsive-lg mt-2 text-center" style="color: black;
+        text-shadow: -1px -1px 0 #fff, 1px -1px 0 #fff, -1px 1px 0 #fff, 1px 1px 0 #fff;">TECHNICAL EXTRAVAGANZA</h1>
+  </div>
+  <div class="pb-5"></div>
+</div>
+
+
+<!-- Display the countdown timer in an element -->
+
+
+<script>
+// Set the date we're counting down to
+var countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
+</script>  
+
+
+  <style>
+    /*Media Queries*/
+	@media (min-width:320px)  { .res-img{width:100%;height:100%;} .responsive-md{font-size:24px;} .responsive-lg{font-size:36px;} .responsive-tx{font-size:16px;} .res-pad{padding:10px;} .res-mar{margin:8px;} /* smartphones, iPhone, portrait 480x320 phones */ }
+	@media (min-width:481px)  { .res-img{width:100%;height:100%;} .responsive-md{font-size:64px;} .responsive-lg{font-size:88px;} .responsive-tx{font-size:36px;} .res-pad{padding:20px;} .res-mar{margin:16px;} /* portrait e-readers (Nook/Kindle), smaller tablets @ 600 or @ 640 wide. */ }
+  @media (min-width:641px)  { .res-img{width:100%;height:100%;} .responsive-md{font-size:64px;} .responsive-lg{font-size:88px;}  .responsive-tx{font-size:36px;} .res-pad{padding:20px;} .res-mar{margin:16px;} /* portrait tablets, portrait iPad, landscape e-readers, landscape 800x480 or 854x480 phones */ }
+	@media (min-width:961px)  { .res-img{width:100%;height:100%;} .responsive-md{font-size:64px;}  .responsive-lg{font-size:88px;} .responsive-tx{font-size:36px;} .res-pad{padding:20px;} .res-mar{margin:16px;} /* tablet, landscape iPad, lo-res laptops ands desktops */ }
+	@media (min-width:1025px) { .res-img{width:100%;height:100%;} .responsive-md{font-size:64px;}  .responsive-lg{font-size:88px;} .responsive-tx{font-size:36px;} .res-pad{padding:20px;} .res-mar{margin:16px;} /* big landscape tablets, laptops, and desktops */ }
+	@media (min-width:1281px) { .res-img{width:100%;height:100%;} .responsive-md{font-size:64px;}  .responsive-lg{font-size:88px;} .responsive-tx{font-size:36px;} .res-pad{padding:20px;} .res-mar{margin:16px;} /* hi-res laptops and desktops */ }
+	
+</style>
+    
+</html>
