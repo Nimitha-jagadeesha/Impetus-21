7b055ffb008a11c3a4e58880018b638076124078
Nimitha jagadeesha <nimitha1jagadeesha@gmail.com>
2021-05-10 09:10:21 +0530

    update

--- events.php
+++ events.php
@@ -332,24 +332,18 @@
 
 <head>
     <meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Impetus 21</title>
     <link rel="icon" href="/logo.png" type="image/x-icon">
-    <!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">-->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
-    <!--font awesome-->
-    <!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">-->
-    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
-    <!--bootstrap js-->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/all.css" />
+    <script src="js/jquery.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
     <!--google font poppins-->
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
-    <link rel="stylesheet" href="snakeGame/main.css">
 </head>
 
 <body style="font-family:'Poppins', sans-serif;background-color:black;">
@@ -383,6 +377,9 @@
                 <div class="col-12 col-md-4 mb-2" ontouchstart="this.classList.toggle('hover');">
                     <div class="container">
                         <div class="front" style="background-image: url('test.jpg');">
+                            <div class="inner">
+
+                            </div>
                         </div>
                         <div class="back">
                             <div class="inner">
@@ -426,7 +423,7 @@
                 </div>
                 <div class="col-12  col-md-4 mb-2" ontouchstart="this.classList.toggle('hover');">
                     <div class="container">
-                    <div class="front" style="background-image: url('test.jpg');">
+                        <div class="front" style="background-image: url('test.jpg');">
                         </div>
                         <div class="back">
                             <div class="inner">
@@ -437,7 +434,7 @@
                 </div>
                 <div class="col-12  col-md-4 mb-2" ontouchstart="this.classList.toggle('hover');">
                     <div class="container">
-                    <div class="front" style="background-image: url('test.jpg');">
+                        <div class="front" style="background-image: url('test.jpg');">
                         </div>
                         <div class="back">
                             <div class="inner">
@@ -448,7 +445,7 @@
                 </div>
                 <div class="col-12  col-md-4 mb-2" ontouchstart="this.classList.toggle('hover');">
                     <div class="container">
-                    <div class="front" style="background-image: url('test.jpg');">
+                        <div class="front" style="background-image: url('test.jpg');">
                         </div>
                         <div class="back">
                             <div class="inner">
@@ -459,7 +456,7 @@
                 </div>
                 <div class="col-12  col-md-4 mb-2" ontouchstart="this.classList.toggle('hover');">
                     <div class="container">
-                    <div class="front" style="background-image: url('test.jpg');">
+                        <div class="front" style="background-image: url('test.jpg');">
                         </div>
                         <div class="back">
                             <div class="inner">
--- index.php
+++ index.php
@@ -130,19 +130,11 @@
     -webkit-transition: all 0.3s 1s ease-out;
     transition: all 0.3s 1s ease-out;
   }
-
-  /*Content*/
-  .content {
-    margin: 0 auto;
-    padding-bottom: 50px;
-    width: 80%;
-    max-width: 978px;
-  }
 </style>
 <script>
   setTimeout(function() {
     $('body').addClass('loaded');
-  }, 1000);
+  },500);
 </script>
 
 <head>
@@ -150,26 +142,19 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Impetus 21</title>
   <link rel="icon" href="/logo.png" type="image/x-icon">
-  <!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">-->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
-  <!--font awesome-->
-  <!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">-->
-  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
-  <!--bootstrap js-->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/all.css" />
+  <script src="js/jquery.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
   <!--google font poppins-->
   <link rel="preconnect" href="https://fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
   <link rel="stylesheet" href="snakeGame/main.css">
 </head>
 
 <body style="font-family:'Poppins', sans-serif;background-color:black;">
   <div id="part1" class="p-2">
-    <!--style="background-color:black;"-->
     <nav class="navbar navbar-expand-md navbar-dark" style="background-color:black;color:white;">
       <a href="#" class="navbar-brand"></a>
       <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
